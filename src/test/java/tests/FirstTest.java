37d0f4684ede7eccca6b63505c73d1b15ac51d30
Nikasvet <svetovav@gmail.com>
2021-08-15 16:21:14 +0300

    added SystemPropertiesTests

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,75 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class FirstTest {
+
+    @BeforeAll
+    static void setup() {
+
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+
+    @Test
+    void positiveFillTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Nika");
+        $("#lastName").setValue("Nika");
+        $("#userEmail").setValue("Nika@nike.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9999999999");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(".react-datepicker__day--011").click();
+
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/test.jpg"));
+        $("#currentAddress").setValue("Testoviy pereulochek");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("#submit").scrollTo().click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Nika Nika"), text("Nika@nike.ru"), text("Male"),
+                text("9999999999"), text("11 November,1991"), text("Testoviy pereulochek"),
+                text("Reading"),  text("Haryana Karnal"), text("test.jpg"));
+
+    }
+
+    }
+
